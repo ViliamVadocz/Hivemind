4b4d2b8792150d36266980662394d1e14b5ac8a3
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-02-10 22:41:30 +0100

    basic rust bot just to test if it works

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -1,22 +1,26 @@
-// use rlbot::ffi;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    rlbot::run_bot(MyBot {
-        index: 0
-    })
+fn main() -> Result<(), Box<dyn Error>> {
+    rlbot::run_bot(MyBot { player_index: 0 })
 }
 
 struct MyBot {
-    index: usize
+    player_index: usize,
 }
 
 impl rlbot::Bot for MyBot {
     fn set_player_index(&mut self, index: usize) {
-        self.index = index;
+        self.player_index = index;
     }
 
     fn tick(&mut self, packet: &rlbot::GameTickPacket) -> rlbot::ControllerState {
-        let ctrl = rlbot::ControllerState::default();
-        ctrl
+        get_input(self.player_index, packet).unwrap_or_default()
     }
+}
+
+fn get_input(player_index: usize, packet: &rlbot::GameTickPacket,) -> Option<rlbot::ControllerState> {
+    Some(rlbot::ControllerState {
+        throttle: 1.0,
+        ..Default::default()
+    })
 }
