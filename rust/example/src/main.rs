ad5959535f88150cfd014d093566bd19392a2c6f
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-02-12 22:19:29 +0100

    Indecisive about project structure with rust

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -1,11 +1,21 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    rlbot::run_bot(MyBot { player_index: 0 })
+    rlbot::run_bot(
+        MyBot {
+            player_index: 0,
+            last_time: 0.0,
+            delta_time: 0.0,
+            timer: 0.0
+        }
+    )
 }
 
 struct MyBot {
     player_index: usize,
+    last_time: f32,
+    delta_time: f32,
+    timer: f32
 }
 
 impl rlbot::Bot for MyBot {
@@ -14,7 +24,22 @@ impl rlbot::Bot for MyBot {
     }
 
     fn tick(&mut self, packet: &rlbot::GameTickPacket) -> rlbot::ControllerState {
-        get_input(self.player_index, packet).unwrap_or_default()
+        let game_time = packet.game_info.seconds_elapsed;
+        self.delta_time = game_time - self.last_time;
+        self.last_time = game_time;
+        self.timer += self.delta_time;
+
+        if self.timer > 1.0 {
+            println!("Running!");
+            self.timer = 0.0;
+        }
+
+        rlbot::ControllerState {
+            throttle: 1.0,
+            steer: 1.0,
+            ..Default::default()
+        }
+        // get_input(self.player_index, packet).unwrap_or_default()
     }
 }
 
